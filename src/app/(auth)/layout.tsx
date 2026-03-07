abc099e17164618c67624e0a1276f7a4e85412a9
lightning-88 <erlanggasatria6677@gmail.com>
2026-03-08 00:27:06 +0700

    fixing unused file from previous merge

--- src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="flex justify-center items-center h-dvh">{children}</div>
-  );
-}
--- src/lib/database/prisma.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
-
-const connectionString = process.env.DATABASE_URL;
-
-const adapter = new PrismaPg({ connectionString });
-
-export const db = new PrismaClient({ adapter });
